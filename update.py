efd228e3f815ce42572aa88f705cd26cab997fe2
Julian Kahnert <mail@juliankahnert.de>
2017-05-06 21:17:43 +0200

    Version 0.44

--- update.py
+++ update.py
@@ -62,7 +62,7 @@ RUN pip install bs4 lxml
 RUN echo "deb http://apt.pilight.org/ stable main" > /etc/apt/sources.list.d/pilight.list && \\
     wget -O - http://apt.pilight.org/pilight.key | apt-key add - && \\
     apt-get update && \\
-    apt-get install -y --no-install-recommends pilight && \\
+    apt-get install -y --force-yes --no-install-recommends pilight && \\
     rm -rf /var/lib/apt/lists/*
 
 EXPOSE 8123
